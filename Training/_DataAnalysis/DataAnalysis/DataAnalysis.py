9862fb4f5eb769774d29d3d5775960ce336dcca4
Jakob <jakob.yde.madsen@gmail.com>
2019-06-20 00:52:21 +0200

    Outputactivation plots

--- Training/_DataAnalysis/DataAnalysis/DataAnalysis.py
+++ Training/_DataAnalysis/DataAnalysis/DataAnalysis.py
@@ -3,9 +3,9 @@ from DataPlotting import *
 import numpy as np
 
 ds = DataSheet('results_0.csv')
-removed = ds.remove_by_performance('last_perf')
+#removed = ds.remove_by_performance('last_perf')
 
-ds = ds.exclude(parameters=['simplernn'])
+#ds = ds.exclude(parameters=['simplernn'])
 
 ds_in = ds.include(categories=['in'])
 ds_out = ds.include(categories=['out'])
