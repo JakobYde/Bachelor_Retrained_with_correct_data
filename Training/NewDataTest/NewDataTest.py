f18c457d16c0cf4a6da0f4f7d8813aafbe48635b
Jakob <jakob.yde.madsen@gmail.com>
2019-06-23 14:07:12 +0200

    newdatatest

--- Training/NewDataTest/NewDataTest.py
+++ Training/NewDataTest/NewDataTest.py
@@ -4,9 +4,6 @@ from keras.models import load_model
 import os
 import csv
 from BachelorUtilities import loadData
-import sys
-
-sys.path.insert(0, r"../..")
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -18,11 +15,12 @@ def testModel(model, x1, x2, y):
     diff = preds - y
     return preds, diff
 
-
 _, tr_crp, _ = loadData('Training')
 eul, crp, das = loadData('New')
 
-model_path = 'TrainModel/TrainModel/models/best_model.h5'
+eul = np.reshape(eul, (eul.shape[0], eul.shape[1], 1))
+
+model_path = r'C:\Users\Jakobs PC\Documents\GitHub\BachelorRetraining\Training\TrainModel\TrainModel\models/best_model.h5'
 
 crp = (crp - np.mean(tr_crp)) / np.std(tr_crp)
 
@@ -44,6 +42,8 @@ diff = preds - das
 
 mae = np.mean(np.abs(diff))
 mse = np.mean(np.square(diff))
+
+print(mae, mse)
 
 with open('results.csv', 'w') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
